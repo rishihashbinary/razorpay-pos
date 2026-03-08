e0de988bab90405eeca4fa8d9f941858a26c7651
Rishi <rishi@hashbinary.com>
2026-03-08 16:41:34 +0530

    Added models for properties

--- app/src/main/java/com/routehub/pos/services/PropertiesService.kt
+++ app/src/main/java/com/routehub/pos/services/PropertiesService.kt
@@ -1,10 +1,11 @@
 package com.routehub.pos.services
 
+import com.routehub.pos.models.responses.PropertyResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-class PropertiesService {
+interface PropertiesService {
 
     @GET("properties/{qr}")
     fun getProperty(
